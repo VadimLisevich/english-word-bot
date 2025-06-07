47923a6624735087717757bac3d54141ab37d6f0
VadimLisevich <vadim.lisevich@gmail.com>
2025-06-07 22:10:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-import asyncio
+import os
 import logging
+import asyncio
 import nest_asyncio
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
+from dotenv import load_dotenv
+from telegram.ext import ApplicationBuilder, MessageHandler, CallbackQueryHandler, CommandHandler, filters
 from core import (
-    start, handle_callback, handle_message_func, menu
+    start, menu, handle_message_func, handle_callback,
+    add_word_command, view_words_command, delete_word_command
 )
 from database import init_db
 from scheduler import schedule_reminders
+
+nest_asyncio.apply()
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 logging.basicConfig(
     format='%(asctime)s | %(levelname)s | %(message)s',
     level=logging.INFO
 )
 
-nest_asyncio.apply()
-init_db()
-
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = getenv("TELEGRAM_BOT_TOKEN")
-
 async def main():
+    init_db()
     application = ApplicationBuilder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("menu", menu))
-    application.add_handler(CallbackQueryHandler(handle_callback))
+    application.add_handler(CommandHandler("add", add_word_command))
+    application.add_handler(CommandHandler("view", view_words_command))
+    application.add_handler(CommandHandler("delete", delete_word_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message_func))
+    application.add_handler(CallbackQueryHandler(handle_callback))
 
     schedule_reminders(application)
     await application.run_polling()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
